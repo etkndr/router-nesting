0d2abe0977e4bf81b4b82f218b1c050c9d196898
eric <etkndr@gmail.com>
2023-03-02 05:39:02 +0000

    Update 4 files

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,14 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom";
+
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+
+  const movie = movies.find((movie) => movie.id === +movieId);
+
   return (
-    <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+    <div className="comp purple">
+      <h1>{movie.title}</h1>
+      <p>{movie.description}</p>
     </div>
   );
 }
